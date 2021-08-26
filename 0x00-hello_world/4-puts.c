86dd20b7334092f835ddda1d6f6c3357e54292d5
Nile Okomo <okomonile3@gmail.com>
2021-08-26 10:02:30 +0300

    Use puts function to print out string characters

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzle, 
- * 		  followed by a new line.
+ * main - Prints "Programming is like building a multilingual puzzle, followed by a new line.
  * 
- * Return: Always 0.
+ * Returns 0.
  */
 int main(void)
 {
