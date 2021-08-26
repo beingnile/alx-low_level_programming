320a7a67309ac1b2d42a80921dfa68ee3d732a6c
Nile Okomo <okomonile3@gmail.com>
2021-08-26 09:42:02 +0300

    Use puts function to print out string characters

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/*
+ * The puts function is in the stdio.h library
+ * It reads characters and prints them out on the screen
+ */
+
+#include <stdio.h>
+
+int main ()
+{
+	puts("\"Programming is like building a multilingual puzzle"\n);
+
+	return(0);
+}
