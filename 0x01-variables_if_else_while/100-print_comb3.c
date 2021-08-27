6593a0de4b3b5a7f86b95a03b743b2403055a8f3
Nile Okomo <okomonile3@gmail.com>
2021-08-27 19:03:05 +0300

    Print possible combinations of single digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,19 +10,20 @@ int main(void)
 {
 	int n, m;
 
-	n = m = '0';
-
-	for (n = '0'; n <= '9'; n++)
+	for (n = '0'; n <= '8'; n++)
 	{
 		for (m = '0'; m <= '9'; m++)
 		{
-			putchar(n);
-			putchar(m);
-
-			if ((n != '9') || (n == '9' && m != '9'))
+			if (n < m)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(n);
+				putchar(m);
+
+				if (n != '8' || (n == '8' && m != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
