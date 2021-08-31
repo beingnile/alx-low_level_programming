7c808309b73ec6beec3723173f7419c892571745
Nile Okomo <okomonile3@gmail.com>
2021-08-31 19:10:33 +0300

    Print multiplication table of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * times_table - Prints the 9 times table, starting from 0.
+ */
+void times_table(void)
+{
+	int num, mult, aug;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			aug = num * mult;
+
+			if (aug <= 9)
+				_putchar(' ');
+			else
+				_putchar((aug / 10) + '0');
+
+			_putchar((aug % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
