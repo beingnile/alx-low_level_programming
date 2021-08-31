f70ec3fd8367cc4e826b98395285fa1d1d29b655
Nile Okomo <okomonile3@gmail.com>
2021-08-31 20:57:37 +0300

    Print sum of even fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Gets the sum of even Fibonnaci numbers up to 4000000.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int i;
+	long int j = 1;
+	long int k = 1;
+	long int sum = 2;
+	long long int total;
+
+	for (i = 1; i < 4000000 ; i++)
+	{
+		sum = j + k;
+		if (sum % 2 == 0)
+			total += sum;
+		j = k;
+		k = sum;
+	}
+
+	printf("%ld\n", total);
+
+	return (0);
+}
