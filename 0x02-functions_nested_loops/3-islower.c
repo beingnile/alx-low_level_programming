dd72ca80414b37ef53d35b4335d99e76d4d1966d
Nile Okomo <okomonile3@gmail.com>
2021-08-31 15:42:59 +0300

    Check if input is a lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * int _islower = Checks if an input is a lowercase letter.
+ *
+ */
+
+int _islower(int c)
+{
+	int islower(int c);
+
+	if (islower(c))
+		return (1);
+	else
+		return (0);
+}
