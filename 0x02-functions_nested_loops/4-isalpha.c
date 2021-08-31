8a923f25e08d994da036b56248b2d105d8fdfd64
Nile Okomo <okomonile3@gmail.com>
2021-08-31 17:47:28 +0300

    Check if character is alphabetic

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 
 /**
- * _isalpha - Checks if a character is uppercase.
+ * _isalpha - Checks if a character is alphabetic.
  * @c: The character to be passed as argument.
  *
- * Return: 1 if character is uppercase, 0 if not.
+ * Return: 1 if character is alphabetic, 0 if not.
  */
 int _isalpha(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
