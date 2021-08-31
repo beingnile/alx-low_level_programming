0bec339b3986f1c6db8656f8daee1c40bd487560
Nile Okomo <okomonile3@gmail.com>
2021-08-31 15:18:53 +0300

    Print lowercase alphabets 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,14 @@ void print_alphabet_x10(void)
 	int i = 0;
 	char alpha;
 
-	while (i <= 10)
+	while (i < 10)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
-			_putchar(alpha)
+			_putchar(alpha);
 		}
 
-		_putchar('\n')
+		_putchar('\n');
+		i++;
 	}
 }
