50b04c0654e658611a94a26263a97fa3b400229d
Nile Okomo <okomonile3@gmail.com>
2021-09-02 12:36:21 +0300

    Fix bug in code

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,11 +9,12 @@
  */
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	/*
 	 * else if ((c >= 'a') && (c <= 'z'))
 	 *	 return (0);
 	 */
-	return (0);
+	else
+		return (0);
 }
