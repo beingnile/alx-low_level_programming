7d4c2fc4adbf470a3868d1192b280fc96dfce918
Nile Okomo <okomonile3@gmail.com>
2021-09-06 19:56:51 +0300

    Copy string pointed at

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-#include "holberton.h"
-
 /**
  * _strcpy - Copies a string pointed to by @src, including the
  *           terminating null byte, to a buffer pointed to by @dest.
