c3369c6ade434f13ce00a2de5293b5e16f59b6cd
Nile Okomo <okomonile3@gmail.com>
2021-09-06 21:31:07 +0300

    Convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,21 +10,21 @@ int _atoi(char *s)
 	int i = 0;
 	int j = 0;
 	int k = 0;
-	int l = 1;
+	int sign = 1;
 	int stop_sign = 0;
 	int stop_num = 0;
 	int ent_num = 0;
-	char num[100];
+	char number_in_array[100];
 
 	while (*(s + i) != '\0')
 	{
 		if (*(s + i) == '+' && stop_sign == 0)
-			l *= 1;
+			sign *= 1;
 		else if (*(s + i) == '-' && stop_sign == 0)
-			l *= -1;
+			sign *= -1;
 		if (*(s + i) >= '0' && *(s + i) <= '9' && stop_num == 0)
 		{
-			num[j] = (*(s + i) - 48);
+			number_in_array[j] = (*(s + i) - 48);
 			stop_sign = 1;
 			ent_num = 1;
 			j++;
@@ -35,16 +35,16 @@ int _atoi(char *s)
 				stop_num = 1;
 			else
 			{
-				num[j] = '0';
-				num[1] = '\0';
+				number_in_array[j] = '0';
+				number_in_array[1] = '\0';
 			}
 		}
 	i++;
 	}
-	printf("sign: %d", l);
-	while (num[k] != '\0')
+	printf("sign: %d", sign);
+	while (number_in_array[k] != '\0')
 	{
-		putchar(num[k]);
+		putchar(number_in_array[k]);
 		k++;
 	}
 	return (0);
