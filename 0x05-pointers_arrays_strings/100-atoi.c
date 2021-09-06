1147e1e2989e87ad235657804606ed0144e6402b
Nile Okomo <okomonile3@gmail.com>
2021-09-06 20:16:17 +0300

    Convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,27 +1,51 @@
 #include "holberton.h"
-
 /**
- * _atoi - Converts a string to an integer.
- * @s: The string to be converted.
+ * _atoi - Get the first integer from a string
+ * @s: String
  *
- * Return: The integer value of the converted string.
- */
+ * Return: The integer obtained
+*/
 int _atoi(char *s)
 {
-	int i = 1;
-	unsigned int num = 0;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int l = 1;
+	int stop_sign = 0;
+	int stop_num = 0;
+	int ent_num = 0;
+	char num[100];
 
-	do {
-		if (*s == '-')
-			i *= -1;
-
-		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
-
-		else if (num > 0)
-			break;
-
-	} while (*s++);
-
-	return (num * i);
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == '+' && stop_sign == 0)
+			l *= 1;
+		else if (*(s + i) == '-' && stop_sign == 0)
+			l *= -1;
+		if (*(s + i) >= '0' && *(s + i) <= '9' && stop_num == 0)
+		{
+			num[j] = (*(s + i) - 48);
+			stop_sign = 1;
+			ent_num = 1;
+			j++;
+		}
+		else
+		{
+			if (ent_num == 1)
+				stop_num = 1;
+			else
+			{
+				num[j] = '0';
+				num[1] = '\0';
+			}
+		}
+	i++;
+	}
+	printf("sign: %d", l);
+	while (num[k] != '\0')
+	{
+		putchar(num[k]);
+		k++;
+	}
+	return (0);
 }
