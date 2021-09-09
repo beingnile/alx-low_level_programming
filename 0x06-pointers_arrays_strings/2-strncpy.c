53e0c2da9fba5ed0df8d2127881db679a569b1e8
Nile Okomo <okomonile3@gmail.com>
2021-09-09 16:20:39 +0300

    Copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,14 +16,14 @@ char *_strncpy(char *dest, const char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n && *(src + i) != '\0'; i++)
 	{
-		dest[i] = src[i];
+		*(dest + i) = *(src + i);
 	}
 	for ( ; i < n; i++)
 	{
-		dest[i] = '\0';
+		*(dest + i) = '\0';
 	}
 
-	return dest;
+	return (dest);
 }
