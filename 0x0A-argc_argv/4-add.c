ceedcbee259ec6b13a68889050ac6f48706934c5
Nile Okomo <okomonile3@gmail.com>
2021-09-18 04:31:51 +0300

    Fix bug in code

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,19 +11,19 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]))
+		for (j = 0; argv[i][j]; j++)
 		{
-			sum += atoi(argv[i]);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else if ((atoi(argv[i])) < '0' || (atoi(argv[i])) > '9')
-		{
-			printf("Error\n");
-			return (1);
-		}
+		sum += atoi(argv[i];
 	}
 
 	printf("%d\n", sum);
