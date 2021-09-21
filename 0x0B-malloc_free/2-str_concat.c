ccd082eae30dea68b1460e4fa914eccfd71c6e74
Nile Okomo <okomonile3@gmail.com>
2021-09-21 22:38:00 +0300

    Concatenate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,7 +20,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i] && s2[i]; i++)
+	for (i = 0; s1[i] || s2[i]; i++)
 		j++;
 
 	s3 = malloc(sizeof(char) * j);
