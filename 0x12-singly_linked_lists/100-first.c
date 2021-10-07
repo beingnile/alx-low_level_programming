42323744468e7c1ec7689d0a3628d8ac93db288d
Nile Okomo <okomonile3@gmail.com>
2021-10-07 18:41:32 +0300

    Print strings before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * first - Prints strings before main function.
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my houose upon my back!\n");
+}
