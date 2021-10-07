8b58b55c6ad9e6cd47fb0a7ecae1207c9c695bbe
Nile Okomo <okomonile3@gmail.com>
2021-10-07 18:43:19 +0300

    Print strings before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,6 @@
  */
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my houose upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
