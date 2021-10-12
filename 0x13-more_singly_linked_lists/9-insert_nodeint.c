6d64c9b0b8a509453c17bab16ccaaf973e985fcd
Nile Okomo <okomonile3@gmail.com>
2021-10-12 20:47:51 +0300

    Add description of function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,14 @@
 #include "lists.h"
 
+/**
+ * insert_nodeint_at_index - Inserts a new node at a given position.
+ * @head: Double pointer to head.
+ * @idx: Index of list where new node should be added.
+ * @n: Int data.
+ *
+ * Return: Address of node, NULL on failure
+ */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new, *cpy = *head;
