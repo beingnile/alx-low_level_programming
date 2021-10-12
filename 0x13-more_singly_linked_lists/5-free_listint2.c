194c476d7940ed2b9ef3bc6a1b61ab2a02c92e27
Nile Okomo <okomonile3@gmail.com>
2021-10-12 19:31:17 +0300

    Fix bug in code

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tempNode;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
 	while (*head)
@@ -19,5 +19,5 @@ void free_listint2(listint_t **head)
 		*head = tempNode;
 	}
 
-	*head = NULL;
+	head = NULL;
 }
