9e959ac40eef4f106496fea60e703e0baaa2b7b5
Nile Okomo <okomonile3@gmail.com>
2021-10-15 01:20:28 +0300

    Update header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,6 @@ int power(int x, int y);
 void print_binary(unsigned long int n);
 int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
