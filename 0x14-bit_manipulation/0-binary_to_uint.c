2920ca0655fb07f062ce9d87582ec6ff6c83044d
Nile Okomo <okomonile3@gmail.com>
2021-10-14 20:46:50 +0300

    Fix bug in code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * str_len - Finds length of string.
+ *
+ * @s: String of which length is to be found.
+ *
+ * Return: The length of the string;
+ */
 int str_len(const char *s)
 {
 	int len = 0, idx;
@@ -13,6 +20,14 @@ int str_len(const char *s)
 	return (len);
 }
 
+/**
+ * power - Finds Power @y of @x.
+ *
+ * @x: Base number.
+ * @y: Exponent.
+ *
+ * Return: x raised to power y.
+ */
 int power(int x, int y)
 {
 	int pow = 0;
@@ -25,12 +40,20 @@ int power(int x, int y)
 	return (pow);
 }
 
+/**
+ * binary_to_uint - Converts binary number to unsigned int.
+ *
+ * @b: Pointer to string of 0 and 1 chars.
+ *
+ * Return: The converted number, 0 if b is NULL.
+ * 	   0 if there's one or more chars in b != 0 or 1.
+ */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int len = str_len(b);
 	unsigned int dec = 0, idx = 0;
 
-	if (b == NULL)
+	if (b == '\0')
 		return (0);
 
 	while (len > 0)
