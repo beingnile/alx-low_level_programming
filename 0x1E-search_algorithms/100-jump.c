286040f010b4d7dc829147e20fe25e092b2811bb
Nile Okomo <okomonile3@gmail.com>
2022-03-30 02:00:34 +0300

    Prototype Jump search algorithm

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,17 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - Searches for a value in a sorted array of
+ *		 integers using the Jump search algorithm.
+ *
+ * @array: Pointer to first element of array to search in
+ * @size: Number of elements in @array
+ * @value: Value to search for
+ *
+ * Return: Index where value was found.
+ *	   -1 if @value or if array == NULL
+ */
+int jump_search(int *array, size_t size, int value)
+{
+
+}
