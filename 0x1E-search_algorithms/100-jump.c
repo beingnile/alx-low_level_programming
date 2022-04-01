84685f339b584245b30c252b37aa7643638ec13d
Nile Okomo <okomonile3@gmail.com>
2022-04-01 19:40:20 +0300

    Implement Jump Search Algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,6 +1,19 @@
 #include "search_algos.h"
 
 /**
+ * min - Compares two values and returns the minimum of the two
+ *
+ * @a: First value
+ * @b: Second value
+ *
+ * Return: The minimum value
+ */
+size_t min(size_t a, size_t b)
+{
+	return ((a > b) ? b : a);
+}
+
+/**
  * jump_search - Searches for a value in a sorted array of
  *		 integers using the Jump search algorithm.
  *
@@ -13,8 +26,27 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int a = 0;
-	int b = sqrt(size);
+	size_t a = 0;
+	size_t b = sqrt(size);
 
 
+	while (array[(min(b, size) - 1)] < value)
+	{
+		printf("Value checked array[%ld] = [%d]\n", a, array[a]);
+		a = b;
+		b = b + sqrt(size);
+		if (a >= size)
+			return (-1);
+	}
+
+	while (array[a] < value)
+	{
+		++a;
+		if (a == min(b, size))
+			return (-1);
+	}
+	if (array[a] == value)
+		return (a);
+	else
+		return (-1);
 }
