801e0835ae0b1ff2f109001029e7f6ca69103624
Nile Okomo <okomonile3@gmail.com>
2024-05-08 22:10:12 +0300

    Add search algorithms directory

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Searches for a value in an array of integers using the
+ * linear serach algorithm. Every compared value is printed on stdout
+ *
+ * @array: Pointer to the first elemet of the array to serach
+ * @size: The number of elements in @array
+ * @value: Value to search for
+ *
+ * Return: The first index where @value is located
+ */
+
